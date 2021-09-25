cafdb6cd37088338d46454de7ae6544bc62fd230
robbiemie <yang657850144@163.com>
2021-09-25 12:39:07 +0800

    feat: ipc 调用

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow, ipcMain } = require("electron")
+
+app.on('ready', () => {
+  // 开发依赖
+  let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule:true,
+      contextIsolation:false
+    }
+  })
+  mainWindow.loadFile('./index.html')
+  // 开发者工具
+  mainWindow.webContents.openDevTools({ mode: 'undocked' });
+  ipcMain.on('message', (event,args) => {
+    console.log(args)
+  })
+})
